de162ba0ae5c388012dc6ec42836ba4b497d6ef9
thomasmolnvik <thomas.molnvik@gmail.com>
2025-10-01 21:11:15 +0200

    ok

--- Studentapp/db.php
+++ Studentapp/db.php
@@ -1,7 +1,7 @@
 <?php
 $host = thmol2568 ("DB_HOST");   // evt. "mysql"
 $username = thmol2568 ("DB_USER");        // endre om nødvendig
-$password = 3509thmol2568 ("DB_PASSWORD");            // endre om nødvendig
+$password = thmol2568 ("DB_PASSWORD");            // endre om nødvendig
 $database   = thmol2568 ("DB_DATABASE";)       // databasenavn
 
 $db=mysqli_connect($host,$user,$pass,$db) or die ("ikke kontakt med database-server");
--- Studentapp/registrere_student.php
+++ Studentapp/registrere_student.php
@@ -1,19 +1,10 @@
 <?php
-<?php /* registrer-student */
-/*
-/* Programmet lager et html-skjema for å registrere en student
-/* Programmet registrerer data i databasen
-*/
-?>
+// ...existing code...
 <h3>Registrer student</h3>
 <form method="post" action="" id="registrerStudentSkjema" name="registrerStudentSkjema">
 Studentnummer <input type="text" id="studentnummer" name="studentnummer" required /> <br/>
 Fornavn <input type="text" id="fornavn" name="fornavn" required /> <br/>
 Etternavn <input type="text" id="etternavn" name="etternavn" required /> <br/>
-Studiumkode <select name="studiumkode" id="studiumkode">
-<?php print("<option value=''>velg studium </option>");
-include("dynamiske-funksjoner.php"); listeboksStudiumkode(); ?>
-</select> <br/>
 <input type="submit" value="Registrer student" id="registrerStudentKnapp" name="registrerStudentKnapp" />
 <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
 </form>
@@ -23,8 +14,7 @@ if (isset($_POST ["registrerStudentKnapp"]))
     $studentnummer = $_POST ["studentnummer"];
     $fornavn = $_POST ["fornavn"];
     $etternavn = $_POST ["etternavn"];
-    $studiumkode = $_POST ["studiumkode"];
-    if (!$studentnummer || !$fornavn || !$etternavn || !$studiumkode)
+    if (!$studentnummer || !$fornavn || !$etternavn)
     {
         print ("Alle felt m&aring; fylles ut");
     }
@@ -40,10 +30,10 @@ if (isset($_POST ["registrerStudentKnapp"]))
         }
         else
         {
-            $sqlSetning = "INSERT INTO student (studentnummer, fornavn, etternavn, studiumkode)
-            VALUES('$studentnummer','$fornavn','$etternavn','$studiumkode');";
+            $sqlSetning = "INSERT INTO student (studentnummer, fornavn, etternavn)
+            VALUES('$studentnummer','$fornavn','$etternavn');";
             mysqli_query($db, $sqlSetning) or die ("ikke mulig &aring; registrere data i databasen");
-            print ("F&oslash;lgende student er n&aring; registrert: $studentnummer $fornavn $etternavn $studiumkode");
+            print ("F&oslash;lgende student er n&aring; registrert: $studentnummer $fornavn $etternavn");
         }
     }
 }

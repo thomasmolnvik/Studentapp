d17148353c03db24dcc8934925ea30cdcdf96e38
thomasmolnvik <thomas.molnvik@gmail.com>
2025-10-06 11:59:59 +0200

    ok

--- Studentapp/db.php
+++ Studentapp/db.php
@@ -1,9 +1,9 @@
 <?php
-$host = thmol2568 ("DB_HOST");   // evt. "mysql"
-$username = thmol2568 ("DB_USER");        // endre om nødvendig
-$password = thmol2568 ("DB_PASSWORD");            // endre om nødvendig
-$database   = thmol2568 ("DB_DATABASE");       // databasenavn
+$host = getenv("DB_HOST");      // eller f.eks. "localhost"
+$username = getenv("DB_USER");  // eller ditt brukernavn
+$password = getenv("DB_PASSWORD"); // eller ditt passord
+$database = getenv("DB_DATABASE"); // eller databasenavn
 
-$db=mysqli_connect($host,$user,$pass,$db) or die ("ikke kontakt med database-server");
-    /* tilkobling til database-serveren utført */
-
+$db = mysqli_connect($host, $username, $password, $database) or die ("ikke kontakt med database-server");
+/* tilkobling til database-serveren utført */
+?>

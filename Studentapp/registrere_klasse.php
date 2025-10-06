b280d1e90fb0a435b1d2ed81b6052c7533b1cffd
thomasmolnvik <thomas.molnvik@gmail.com>
2025-10-06 12:47:52 +0200

    ok

--- Studentapp/registrere_klasse.php
+++ Studentapp/registrere_klasse.php
@@ -6,6 +6,7 @@
 <h3>Registrer klasse </h3>
 <form method="post" action="" id="registrerKlasseSkjema" name="registrerKlasseSkjema">
 Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
+Klassenavn <input type="text" id="klassenavn" name="klassenavn" required /> <br/>
 <input type="submit" value="Registrer klasse" id="registrerKlasseKnapp" name="registrerKlasseKnapp" />
 <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
 </form>
@@ -13,8 +14,9 @@ Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/
 if (isset($_POST ["registrerKlasseKnapp"]))
 {
     $klassekode = $_POST ["klassekode"];
+    $klassenavn = $_POST ["klassenavn"];
 
-    if (!$klassekode)
+    if (!$klassekode || !$klassenavn)
     {
         print ("Alle felt m&aring; fylles ut");
     }
@@ -30,11 +32,11 @@ if (isset($_POST ["registrerKlasseKnapp"]))
         }
         else
         {
-            $sqlSetning = "INSERT INTO klasse (klassekode)
-            VALUES('$klassekode');";
+            $sqlSetning = "INSERT INTO klasse (klassekode, klassenavn)
+            VALUES('$klassekode','$klassenavn');";
             mysqli_query($db, $sqlSetning) or die ("ikke mulig &aring; registrere data i databasen");
             /* SQL-setning sendt til database-serveren */
-            print ("F&oslash;lgende klasse er n&aring; registrert: $klassekode");
+            print ("F&oslash;lgende klasse er n&aring; registrert: $klassekode $klassenavn");
         }
     }
 }

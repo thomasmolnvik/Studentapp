1fcdc352edb827c217a1a9d9de13f9eb50448679
thomasmolnvik <thomas.molnvik@gmail.com>
2025-10-07 14:07:09 +0200

    ok

--- Studentapp/vis_klasse.php
+++ Studentapp/vis_klasse.php
@@ -10,12 +10,13 @@ $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente dat
 $antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
 print ("<h3>Registrerte klasser </h3>");
 print ("<table border=1>");
-print ("<tr><th align=left>klassekode</th> <th align=left>klassenavn</th> </tr>");
+print ("<tr><th align=left>klassekode</th> <th align=left>klassenavn</th> <th align=left>studiumkode</th></tr>");
 for ($r=1;$r<=$antallRader;$r++)
 {
     $rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
     $klassekode=$rad["klassekode"];
     $klassenavn=$rad["klassenavn"];
-    print ("<tr> <td> $klassekode </td> <td> $klassenavn </td> </tr>");
+    $studiumkode=$rad["studiumkode"];
+    print ("<tr> <td> $klassekode </td> <td> $klassenavn </td> <td> $studiumkode </td> </tr>");
 }
 ?>

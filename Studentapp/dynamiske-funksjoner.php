6d31373848bffe7d9026845968e323430f379baa
thomasmolnvik <thomas.molnvik@gmail.com>
2025-10-07 13:58:00 +0200

    ok

--- Studentapp/dynamiske-funksjoner.php
+++ Studentapp/dynamiske-funksjoner.php
@@ -20,25 +20,3 @@ function listeboksStudentnummer() {
     }
 }
 ?>
-<?php
-function listeboksKlassekode() {
-    include("db.php");
-    $sqlSetning = "SELECT klassekode FROM klasse ORDER BY klassekode;";
-    $sqlResultat = mysqli_query($db, $sqlSetning);
-    while ($rad = mysqli_fetch_array($sqlResultat)) {
-        $klassekode = $rad["klassekode"];
-        print("<option value='$klassekode'>$klassekode</option>");
-    }
-}
-?>
-<?php
-function listeboksStudiumkode() {
-    include("db.php");
-    $sqlSetning = "SELECT studiumkode FROM studium ORDER BY studiumkode;";
-    $sqlResultat = mysqli_query($db, $sqlSetning);
-    while ($rad = mysqli_fetch_array($sqlResultat)) {
-        $studiumkode = $rad["studiumkode"];
-        print("<option value='$studiumkode'>$studiumkode</option>");
-    }
-}
-?>

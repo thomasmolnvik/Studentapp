a00f7c51937898d3b9e86a3b0a86c8c6d2c8e01b
thomasmolnvik <thomas.molnvik@gmail.com>
2025-10-07 13:06:09 +0200

    ok

--- Studentapp/vis_student.php
+++ Studentapp/vis_student.php
@@ -4,20 +4,19 @@
 /* Programmet skriver ut alle registrerte studenter
 */
 include("db.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
-$sqlSetning="SELECT * FROM student ORDER BY studentnummer;";
-$sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen"); /*
-SQL-setning sendt til database-serveren */
-$antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
+$sqlSetning="SELECT * FROM student ORDER BY brukernavn;";
+$sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+$antallRader=mysqli_num_rows($sqlResultat);
 print ("<h3>Registrerte studenter </h3>");
 print ("<table border=1>");
-print ("<tr><th align=left>studentnummer</th> <th align=left>fornavn</th> <th align=left>etternavn</th> <th align=left>klasse</th></tr>");
+print ("<tr><th align=left>Brukernavn</th> <th align=left>Fornavn</th> <th align=left>Etternavn</th> <th align=left>Klassekode</th></tr>");
 for ($r=1;$r<=$antallRader;$r++)
 {
-$rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
-$studentnummer=$rad["studentnummer"];
-$fornavn=$rad["fornavn"];
-$etternavn=$rad["etternavn"];
-$klasse=$rad["klasse"];
-print ("<tr> <td> $studentnummer </td> <td> $fornavn </td> <td> $etternavn </td> <td> $klasse </td> </tr>");
+    $rad=mysqli_fetch_array($sqlResultat);
+    $brukernavn=$rad["brukernavn"];
+    $fornavn=$rad["fornavn"];
+    $etternavn=$rad["etternavn"];
+    $klassekode=$rad["klassekode"];
+    print ("<tr> <td> $brukernavn </td> <td> $fornavn </td> <td> $etternavn </td> <td> $klassekode </td> </tr>");
 }
 ?>
